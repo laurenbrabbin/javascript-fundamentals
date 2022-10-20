1a0e67ecd177649a6cbd6234f8286dd950c2ebff
Lauren Brabbin <laurenbrabbin@MASTUDENT-Lauren-Brabbin-C17G44KUQ6L4.local>
2022-10-20 10:12:06 +0100

    adding weatherView method

--- weather-program/weather.js
+++ weather-program/weather.js
@@ -15,6 +15,16 @@ class Weather {
       console.log(weatherData)
     });
   }
+
+  Weatherview() {
+    return this.api.fetchWeatherData(this.city, (weatherData) => {
+      console.log(`City: ${this.city}`)
+      console.log(`Weather: ${weatherData.weather[0].main}`)
+      console.log(`Tempreture: ${weatherData.main.temp}`)
+      console.log(`Feels like: ${weatherData.main.feels_like}`)
+      console.log(`Humidity: ${weatherData.main.humidity}`)
+    });
+  }
 }
 
 
